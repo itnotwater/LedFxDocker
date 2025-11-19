266bcbfe3608a5f54389549ff976b8a7a63dcde7
itnotwater <762water@proton.me>
2025-11-19 15:11:54 -0500

    Update entrypoint.sh

--- setup-files/entrypoint.sh
+++ setup-files/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Start avahi daemon for WLED auto discovery
-avahi-daemon --daemonize --no-drop-root
+avahi-daemon --daemonize --no-drop-root || true
 
 # https://superuser.com/questions/1539634/pulseaudio-daemon-wont-start-inside-docker
 # Start the pulseaudio server
 rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit || true
 
 if [[ -v FORMAT ]]; then
     ./pipe-audio.sh
 fi
 
 if [[ -v HOST ]]; then
-    snapclient --host "$HOST" --daemon 1
+    snapclient --host "$HOST" &
 fi
 
 if [[ -v SQUEEZE ]]; then
@@ -24,4 +24,4 @@ mkdir /app/ledfx-config
 
 mv -vn /app/config.yaml /app/ledfx-config/
 mkdir /root/.ledfx
-ledfx -c /app/ledfx-config 
+ledfx -c /app/ledfx-config
